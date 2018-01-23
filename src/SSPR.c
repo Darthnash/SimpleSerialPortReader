5b0ad61304dd828bb261fc1f0ea57a72a6a4693a
Lukas Hägele <lukas.haegele93@web.de>
2018-01-23 21:41:53 +0100

    cleanup of SSPR

--- src/SSPR.c
+++ src/SSPR.c
@@ -28,10 +28,8 @@ static int fd;  // file descriptor of serial port
 int main ()
 {
 	/* open port */
-// const char *portname = "/dev/ttyACM0";  // COM Port of connected device
-	const char *portname = "/dev/ttyUSB0";
+	const char *portname = "/dev/ttyUSB0";  // COM Port of connected device
 	fd = open(portname, O_RDONLY | O_NOCTTY | O_NONBLOCK);  // open Port for reading and catch return value, returns descriptor of new file
-	// fd = open(portname, O_RDONLY | O_NOCTTY);
 
 	/* error handling */
 	if (fd < 0)
@@ -41,45 +39,18 @@ int main ()
 	}
 	else
 	{
-		printf("%s: opened.\n", portname);
+		printf("%s: opened.\n", portname);  // display success
 	}
 
 	/* terminal control */
 	struct termios tty;  // declare teletypewriter/terminal variable
 	memset(&tty, 0, sizeof(tty));  // fill memory space of tty with 0 (reset values)
 
-	/* error handling */
-	// if(tcgetattr(fd, &tty) != 0)  // tcgetattr: get parameters that are associated with the terminal and store them in termios structure
-	// {
-	// 	fprintf(stderr, "error %d from tcgetattr", errno);  // display error code
-	// 	exit(-1); // terminate program
-	// }
-	// else
-	// {
-	// 	printf("tcgetattr() succeeded.\n");
-	// }
-
 	/* configure terminal */
 	cfsetispeed(&tty, B19200);  // set input speed to Baudrate 19,200
 
 	// basic terminal input control
-	tty.c_iflag = IGNPAR;
-  //
-	// // basic terminal output control
-	// tty.c_oflag = 0;
-  //
-	// // hardware control of the terminal
-	// tty.c_cflag |= (CS8 | CREAD | CLOCAL);  // force 8bit input, enable receiver, ignore modem conrol lines
-  //
-	// // control various terminal functions
-	// tty.c_lflag = ICANON;
-  //
-	// // control characters
-	// tty.c_cc[VEOF]	= 4;  // Ctrl-d
-	// tty.c_cc[VMIN]	= 1;  // read() is satisfied with # character byte(s)
-	// tty.c_cc[VTIME] = 0;  // inter-character timer off
-
-
+	tty.c_iflag = IGNPAR;  // ignore parity bit
 
 	/* error handling */
 	if(tcsetattr(fd, TCSANOW, &tty) != 0)  // change will occur immediately
@@ -89,7 +60,7 @@ int main ()
 	}
 	else
 	{
-		printf("tcsetattr() succeeded\n");
+		printf("tcsetattr() succeeded\n");  // display success
 	}
 
 	/* signal handler */
@@ -138,13 +109,6 @@ int main ()
 
 
 /* signal handler */
-void signal_handler_IO (int status)
-{
-	printf("SIGIO..."); fflush(stdout);
-	// printf("received SIGIO signal.\n");
-	wait_flag = FALSE;  // indicate reception of characters"
-}
-
 void signal_int (int signum)
 {
 	printf("\nclosing file descriptor.\n");
